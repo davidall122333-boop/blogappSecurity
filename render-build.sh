aceb94b83af5002cd0accbe0da5d0182841f6cfb
atsel <davidall122333@gmail.com>
2026-01-14 08:27:00 +0000

    first commit

--- render-build.sh
+++ render-build.sh
@@ -2,5 +2,14 @@
 # Install deps
 pip install -r requirements.txt
 
+
+set -o errexit
+
+echo "Collecte des fichiers statiques"
+python manage.py collectstatic --noinput
+
+echo "Build terminé avec succès"
+
+
 # Run migrations automatically
 python manage.py migrate
